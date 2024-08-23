a6baa9feb67f411a2bbd765a95b967486edb58f4
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-08-23 17:57:07 -0300

    update,agrego test de exeption

--- test_main.py
+++ test_main.py
@@ -4,61 +4,78 @@ from chess import Chess
 from main import play
 
 
-class TestCli(unittest.TestCase):
-    @patch(  
+class TestMain(unittest.TestCase):
+    @patch(
         'builtins.input',
-        side_effect=['1', '1', '2', '2'], 
+        side_effect=['1', '1', '2', '2'],
     )
-    @patch('builtins.print') 
+    @patch('builtins.print')
     @patch.object(Chess, 'move')
     def test_happy_path(
         self,
         mock_chess_move,
         mock_print,
         mock_input,
-    ): #
+    ):
         chess = Chess()
         play(chess)
         self.assertEqual(mock_input.call_count, 4)
         self.assertEqual(mock_print.call_count, 2)
         self.assertEqual(mock_chess_move.call_count, 1)
 
-    @patch(  
+    @patch(
         'builtins.input',
-        side_effect=['hola', '1', '2', '2'], 
+        side_effect=['hola', '1', '2', '2'],
     )
-    @patch('builtins.print') 
+    @patch('builtins.print')
     @patch.object(Chess, 'move')
     def test_not_happy_path(
         self,
         mock_chess_move,
         mock_print,
         mock_input,
-    ): #
+    ):
         chess = Chess()
         play(chess)
         self.assertEqual(mock_input.call_count, 1)
         self.assertEqual(mock_print.call_count, 3)
         self.assertEqual(mock_chess_move.call_count, 0)
+        mock_print.assert_any_call("Ingrsar numero valido en filas y columnas.")
 
-    @patch(  
+    @patch(
         'builtins.input',
-        side_effect=['1', '1', '2', 'hola'], 
+        side_effect=['1', '1', '2', 'hola'],
     )
-    @patch('builtins.print') 
+    @patch('builtins.print')
     @patch.object(Chess, 'move')
     def test_more_not_happy_path(
         self,
         mock_chess_move,
         mock_print,
         mock_input,
-    ): 
+    ):
         chess = Chess()
         play(chess)
         self.assertEqual(mock_input.call_count, 4)
         self.assertEqual(mock_print.call_count, 3)
         self.assertEqual(mock_chess_move.call_count, 0)
+        mock_print.assert_any_call("Ingrsar numero valido en filas y columnas.")
+
+    @patch('builtins.input', side_effect=KeyboardInterrupt)
+    @patch('builtins.print')
+    @patch.object(Chess, 'move')
+    def test_keyboard_interrupt(
+        self,
+        mock_chess_move,
+        mock_print,
+        mock_input,
+    ):
+        chess = Chess()
+        with self.assertRaises(SystemExit):
+            play(chess)
+        mock_print.assert_any_call("\nJuego interrumpido por el usuario")
+        self.assertEqual(mock_chess_move.call_count, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
-
