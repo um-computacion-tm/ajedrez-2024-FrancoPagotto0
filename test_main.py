aaf87a068888f7e552d3b7067f40153b0347ffe2
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-08-23 18:26:13 -0300

    update,correcion y agrego test

--- test_main.py
+++ test_main.py
@@ -77,5 +77,19 @@ class TestMain(unittest.TestCase):
         self.assertEqual(mock_chess_move.call_count, 0)
 
 
+    @patch('builtins.input', side_effect=['1', '1', '2', '2'])
+    @patch('builtins.print')
+    @patch.object(Chess, 'move', side_effect=Exception("Test Exception"))
+    def test_general_exception(
+        self,
+        mock_chess_move,
+        mock_print,
+        mock_input,
+    ):
+        chess = Chess()
+        play(chess)
+        #verifica que el print  sea llamado con los dos  argumentos correctos
+        calls = [call for call in mock_print.call_args_list if "Ocurrio un error:" in call[0]]
+        self.assertEqual(mock_chess_move.call_count, 1)
 if __name__ == '__main__':
     unittest.main()
