6e0e044a312cd46a4087a27aaedb58c561cea6ff
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-08-23 17:48:43 -0300

    Update de test de main

--- test_main.py
+++ test_main.py
@@ -3,43 +3,62 @@ from unittest.mock import patch
 from chess import Chess
 from main import play
 
-class TestMainS(unittest.TestCase):
-    
-    def run_test_scenario(self, input_values, expected_input_calls, expected_print_calls, expected_move_calls):
-        with patch('builtins.input', side_effect=input_values) as mock_input, \
-             patch('builtins.print') as mock_print, \
-             patch.object(Chess, 'move') as mock_chess_move:
-            
-            chess = Chess()
-            play(chess)
-            
-            self.assertEqual(mock_input.call_count, expected_input_calls)
-            self.assertEqual(mock_print.call_count, expected_print_calls)
-            self.assertEqual(mock_chess_move.call_count, expected_move_calls)
 
-    def test_happy_path(self):
-        self.run_test_scenario(
-            input_values=['1', '1', '2', '2'],
-            expected_input_calls=4,
-            expected_print_calls=2,
-            expected_move_calls=1
-        )
+class TestCli(unittest.TestCase):
+    @patch(  
+        'builtins.input',
+        side_effect=['1', '1', '2', '2'], 
+    )
+    @patch('builtins.print') 
+    @patch.object(Chess, 'move')
+    def test_happy_path(
+        self,
+        mock_chess_move,
+        mock_print,
+        mock_input,
+    ): #
+        chess = Chess()
+        play(chess)
+        self.assertEqual(mock_input.call_count, 4)
+        self.assertEqual(mock_print.call_count, 2)
+        self.assertEqual(mock_chess_move.call_count, 1)
 
-    def test_not_happy_path(self):
-        self.run_test_scenario(
-            input_values=['hola', '1', '2', '2'],
-            expected_input_calls=1,
-            expected_print_calls=3,
-            expected_move_calls=0
-        )
+    @patch(  
+        'builtins.input',
+        side_effect=['hola', '1', '2', '2'], 
+    )
+    @patch('builtins.print') 
+    @patch.object(Chess, 'move')
+    def test_not_happy_path(
+        self,
+        mock_chess_move,
+        mock_print,
+        mock_input,
+    ): #
+        chess = Chess()
+        play(chess)
+        self.assertEqual(mock_input.call_count, 1)
+        self.assertEqual(mock_print.call_count, 3)
+        self.assertEqual(mock_chess_move.call_count, 0)
 
-    def test_more_not_happy_path(self):
-        self.run_test_scenario(
-            input_values=['1', '1', '2', 'hola'],
-            expected_input_calls=4,
-            expected_print_calls=3,
-            expected_move_calls=0
-        )
+    @patch(  
+        'builtins.input',
+        side_effect=['1', '1', '2', 'hola'], 
+    )
+    @patch('builtins.print') 
+    @patch.object(Chess, 'move')
+    def test_more_not_happy_path(
+        self,
+        mock_chess_move,
+        mock_print,
+        mock_input,
+    ): 
+        chess = Chess()
+        play(chess)
+        self.assertEqual(mock_input.call_count, 4)
+        self.assertEqual(mock_print.call_count, 3)
+        self.assertEqual(mock_chess_move.call_count, 0)
 
 if __name__ == '__main__':
     unittest.main()
+
