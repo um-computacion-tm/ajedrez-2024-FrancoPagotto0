191e90b4ab18c198ed56467890debb76fa5e6151
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-08-24 13:18:20 -0300

    update test_chess.py

--- test_chess.py
+++ test_chess.py
@@ -1,20 +1,30 @@
 import unittest
+from unittest.mock import MagicMock
+from board import Board
 from chess import Chess
 
 class TestChess(unittest.TestCase):
 
-    def test_change_turn_white_to_black(self):
+    def test_is_playing(self):
         chess_game = Chess()
-        self.assertEqual(chess_game.turn, "WHITE")
-        chess_game.change_turn()
-        self.assertEqual(chess_game.turn, "BLACK")
+        self.assertTrue(chess_game.is_playing())
 
-    def test_change_turn_black_to_white(self):
+    def test_change_turn(self):
         chess_game = Chess()
+        self.assertEqual(chess_game.turn, "WHITE")
         chess_game.change_turn()
         self.assertEqual(chess_game.turn, "BLACK")
         chess_game.change_turn()
         self.assertEqual(chess_game.turn, "WHITE")
+
+    def test_move_get_piece_and_change_turn(self):
+        chess_game = Chess()
+        mock_board = MagicMock(spec=Board)
+        chess_game._Chess__board__ = mock_board
+        chess_game.move(0, 1, 0, 2)
+        print("Calls to get_piece:", mock_board.get_piece.call_args_list)
+        self.assertEqual(chess_game.turn, "BLACK")
 
 if __name__ == '__main__':
     unittest.main()
+
