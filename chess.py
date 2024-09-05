27f359e7df7858485d152311f4d925a15b36d0cc
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-09-05 11:11:48 -0300

    Update,chess.py

--- chess.py
+++ chess.py
@@ -1,5 +1,6 @@
 from board import Board
-
+from exceptions import InvalidMove, InvalidTurn, EmptyPosition
+ 
 class Chess:
 
     def __init__(self):
@@ -17,6 +18,14 @@ class Chess:
         to_col,
     ):
         piece = self.__board__.get_piece(from_row, from_col)
+        self.change_turn()
+        if not piece:
+            raise EmptyPosition()
+        if not piece.get_color() == self.__turn__:
+            raise InvalidTurn()
+        if not piece.valid_positions(from_row, from_col, to_row, to_col):
+            raise InvalidMove()
+        self.__board__.move(from_row, from_col, to_row, to_col)
         self.change_turn()
     @property
 
