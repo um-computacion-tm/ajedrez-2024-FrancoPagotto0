7e5dd25495f9dfa855fe604f947473908b2d724a
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-09-23 12:06:05 -0300

    Update,edito horse y agrego movimientos a bishop

--- bishop.py
+++ bishop.py
@@ -1,8 +1,82 @@
 from piece import Piece
 
+from piece import Piece
+
 class Bishop(Piece):
-    def __str__(self):
-        if self.color == "WHITE":
-            return "♝"
-        else:
-            return "♗"
+    white_str = "♝"
+    black_str = "♗"
+
+    def valid_positions(
+        self,
+        from_row,
+        from_col,
+        to_row,
+        to_col,
+    ):
+        possible_positions = (
+            self.possible_positions_dd(from_row, from_col) +  
+            self.possible_positions_da(from_row, from_col) +  
+            self.possible_positions_id(from_row, from_col) +  
+            self.possible_positions_ia(from_row, from_col)   
+        )
+        return (to_row, to_col) in possible_positions
+
+    def possible_positions_dd(self, row, col):
+        possibles = []
+        for offset in range(1, 8):
+            new_row = row + offset
+            new_col = col + offset
+            if new_row >= 8 or new_col >= 8:
+                break
+            other_piece = self.__board__.get_piece(new_row, new_col)
+            if other_piece is not None:
+                if other_piece.get_color() != self.get_color():
+                    possibles.append((new_row, new_col))
+                break
+            possibles.append((new_row, new_col))
+        return possibles
+
+    def possible_positions_da(self, row, col):
+        possibles = []
+        for offset in range(1, 8):
+            new_row = row + offset
+            new_col = col - offset
+            if new_row >= 8 or new_col < 0:
+                break
+            other_piece = self.__board__.get_piece(new_row, new_col)
+            if other_piece is not None:
+                if other_piece.get_color() != self.get_color():
+                    possibles.append((new_row, new_col))
+                break
+            possibles.append((new_row, new_col))
+        return possibles
+
+    def possible_positions_id(self, row, col):
+        possibles = []
+        for offset in range(1, 8):
+            new_row = row - offset
+            new_col = col + offset
+            if new_row < 0 or new_col >= 8:
+                break
+            other_piece = self.__board__.get_piece(new_row, new_col)
+            if other_piece is not None:
+                if other_piece.get_color() != self.get_color():
+                    possibles.append((new_row, new_col))
+                break
+            possibles.append((new_row, new_col))
+        return possibles
+
+    def possible_positions_ia(self, row, col):
+        possibles = []
+        for offset in range(1, 8):
+            new_row = row - offset
+            new_col = col - offset
+            if new_row < 0 or new_col < 0:
+                break
+            other_piece = self.__board__.get_piece(new_row, new_col)
+            if other_piece is not None:
+                if other_piece.get_color() != self.get_color():
+                    possibles.append((new_row, new_col))
+                break
+            possibles.append((new_row, new_col))
+        return possibles
--- horse.py
+++ horse.py
@@ -3,7 +3,7 @@ from piece import Piece
 class Horse(Piece):
     white_str = "♞"
     black_str = "♘"
-    
+
     def valid_positions(
         self,
         from_row,
@@ -11,25 +11,61 @@ class Horse(Piece):
         to_row,
         to_col,
     ):
-        possible_positions = self.possible_positions_horse(from_row, from_col)
+        possible_positions = (
+            self.possible_positions_up_right(from_row, from_col) +   
+            self.possible_positions_up_left(from_row, from_col) +    
+            self.possible_positions_down_right(from_row, from_col) + 
+            self.possible_positions_down_left(from_row, from_col)    
+        )
         return (to_row, to_col) in possible_positions
 
-    def possible_positions_horse(self, row, col):
+    def possible_positions_up_right(self, row, col):
         possibles = []
-        # Movimientos del caballo (en forma de "L")
-        horse_moves = [
-            (2, 1), (2, -1), (-2, 1), (-2, -1),
-            (1, 2), (1, -2), (-1, 2), (-1, -2)
-        ]
-        
+        horse_moves = [(2, 1), (1, 2)]  
         for move in horse_moves:
             new_row = row + move[0]
             new_col = col + move[1]
-            if 0 <= new_row < 8 and 0 <= new_col < 8:  # Dentro del tablero
+            if 0 <= new_row < 8 and 0 <= new_col < 8:
                 other_piece = self.__board__.get_piece(new_row, new_col)
                 if other_piece is None or other_piece.get_color() != self.get_color():
                     possibles.append((new_row, new_col))
-        
         return possibles
+
+    def possible_positions_up_left(self, row, col):
+        possibles = []
+        horse_moves = [(2, -1), (1, -2)]  
+        for move in horse_moves:
+            new_row = row + move[0]
+            new_col = col + move[1]
+            if 0 <= new_row < 8 and 0 <= new_col < 8:
+                other_piece = self.__board__.get_piece(new_row, new_col)
+                if other_piece is None or other_piece.get_color() != self.get_color():
+                    possibles.append((new_row, new_col))
+        return possibles
+
+    def possible_positions_down_right(self, row, col):
+        possibles = []
+        horse_moves = [(-2, 1), (-1, 2)]
+        for move in horse_moves:
+            new_row = row + move[0]
+            new_col = col + move[1]
+            if 0 <= new_row < 8 and 0 <= new_col < 8:
+                other_piece = self.__board__.get_piece(new_row, new_col)
+                if other_piece is None or other_piece.get_color() != self.get_color():
+                    possibles.append((new_row, new_col))
+        return possibles
+
+    def possible_positions_down_left(self, row, col):
+        possibles = []
+        horse_moves = [(-2, -1), (-1, -2)]  
+        for move in horse_moves:
+            new_row = row + move[0]
+            new_col = col + move[1]
+            if 0 <= new_row < 8 and 0 <= new_col < 8:
+                other_piece = self.__board__.get_piece(new_row, new_col)
+                if other_piece is None or other_piece.get_color() != self.get_color():
+                    possibles.append((new_row, new_col))
+        return possibles
+
 
    
