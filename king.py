23279b3f6e77d6e613d2b9dc22bba528fce2c0f6
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-09-29 12:06:04 -0300

    Correccion de codigo

--- king.py
+++ king.py
@@ -11,40 +11,17 @@ class King(Piece):
         to_row,
         to_col,
     ):
-        possible_positions = (
-            self.possible_positions_vertical(from_row, from_col) +
-            self.possible_positions_horizontal(from_row, from_col) +
-            self.possible_positions_diagonal(from_row, from_col)
-        )
+        directions = [
+            (1, 0), (0, 1), (0, -1), (-1, 0),  # Vertical and Horizontal
+            (1, 1), (1, -1), (-1, 1), (-1, -1)  # Diagonals
+        ]
+        possible_positions = self.possible_moves(from_row, from_col, directions)
         return (to_row, to_col) in possible_positions
 
-    def possible_positions_vertical(self, row, col):
-        possibles = []
-        for direction in [1, -1]:  # Movimientos hacia arriba y abajo
-            next_row = row + direction
-            if 0 <= next_row < 8:
-                other_piece = self.__board__.get_piece(next_row, col)
-                if other_piece is None or other_piece.get_color() != self.get_color():
-                    possibles.append((next_row, col))
-        return possibles
-
-    def possible_positions_horizontal(self, row, col):
-        possibles = []
-        for direction in [1, -1]:  # Movimientos hacia la derecha y la izquierda
-            next_col = col + direction
-            if 0 <= next_col < 8:
-                other_piece = self.__board__.get_piece(row, next_col)
-                if other_piece is None or other_piece.get_color() != self.get_color():
-                    possibles.append((row, next_col))
-        return possibles
-
-    def possible_positions_diagonal(self, row, col):
-        possibles = []
-        for direction in [(1, 1), (1, -1), (-1, 1), (-1, -1)]:  # Movimientos diagonales
-            next_row = row + direction[0]
-            next_col = col + direction[1]
-            if 0 <= next_row < 8 and 0 <= next_col < 8:
-                other_piece = self.__board__.get_piece(next_row, next_col)
-                if other_piece is None or other_piece.get_color() != self.get_color():
-                    possibles.append((next_row, next_col))
-        return possibles
+    def possible_moves(self, from_row, from_col, directions):
+        moves = []
+        for dr, dc in directions:
+            new_row, new_col = from_row + dr, from_col + dc
+            if 0 <= new_row < 8 and 0 <= new_col < 8:
+                moves.append((new_row, new_col))
+        return moves
