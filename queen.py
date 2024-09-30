42576c38287e3f792580d9a4154cb4d0bf53b186
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-09-30 11:45:41 -0300

    Correccion de codigo

--- queen.py
+++ queen.py
@@ -11,123 +11,24 @@ class Queen(Piece):
         to_row,
         to_col,
     ):
-        possible_positions = (
-            self.possible_positions_vd(from_row, from_col) +
-            self.possible_positions_va(from_row, from_col) +
-            self.possible_positions_hd(from_row, from_col) +
-            self.possible_positions_ha(from_row, from_col) +
-            self.possible_positions_dd(from_row, from_col) +
-            self.possible_positions_da(from_row, from_col) +
-            self.possible_positions_id(from_row, from_col) +
-            self.possible_positions_ia(from_row, from_col)
-        )
+        directions = [
+            (1, 0), (0, 1), (0, -1), (-1, 0),  # Vertical and Horizontal
+            (1, 1), (1, -1), (-1, 1), (-1, -1)  # Diagonals
+        ]
+        possible_positions = self.possible_moves(from_row, from_col, directions, single_step=False)
         return (to_row, to_col) in possible_positions
 
-    # Vertical abajo
-    def possible_positions_vd(self, row, col):
-        possibles = []
-        for next_row in range(row + 1, 8):
-            other_piece = self.__board__.get_piece(next_row, col)
-            if other_piece is not None:
-                if other_piece.get_color() != self.get_color():
-                    possibles.append((next_row, col))
-                break
-            possibles.append((next_row, col))
-        return possibles
-
-    # Vertical arriba
-    def possible_positions_va(self, row, col):
-        possibles = []
-        for next_row in range(row - 1, -1, -1):
-            other_piece = self.__board__.get_piece(next_row, col)
-            if other_piece is not None:
-                if other_piece.get_color() != self.get_color():
-                    possibles.append((next_row, col))
-                break
-            possibles.append((next_row, col))
-        return possibles
-
-    # Horizontal derecha
-    def possible_positions_hd(self, row, col):
-        possibles = []
-        for next_col in range(col + 1, 8):
-            other_piece = self.__board__.get_piece(row, next_col)
-            if other_piece is not None:
-                if other_piece.get_color() != self.get_color():
-                    possibles.append((row, next_col))
-                break
-            possibles.append((row, next_col))
-        return possibles
-
-    # Horizontal izq
-    def possible_positions_ha(self, row, col):
-        possibles = []
-        for next_col in range(col - 1, -1, -1):
-            other_piece = self.__board__.get_piece(row, next_col)
-            if other_piece is not None:
-                if other_piece.get_color() != self.get_color():
-                    possibles.append((row, next_col))
-                break
-            possibles.append((row, next_col))
-        return possibles
-
-    # Diagonal abajo-derecha
-    def possible_positions_dd(self, row, col):
-        possibles = []
-        next_row, next_col = row + 1, col + 1
-        while next_row < 8 and next_col < 8:
-            other_piece = self.__board__.get_piece(next_row, next_col)
-            if other_piece is not None:
-                if other_piece.get_color() != self.get_color():
-                    possibles.append((next_row, next_col))
-                break
-            possibles.append((next_row, next_col))
-            next_row += 1
-            next_col += 1
-        return possibles
-
-    # Diagonal abajo izq
-    def possible_positions_da(self, row, col):
-        possibles = []
-        next_row, next_col = row + 1, col - 1
-        while next_row < 8 and next_col >= 0:
-            other_piece = self.__board__.get_piece(next_row, next_col)
-            if other_piece is not None:
-                if other_piece.get_color() != self.get_color():
-                    possibles.append((next_row, next_col))
-                break
-            possibles.append((next_row, next_col))
-            next_row += 1
-            next_col -= 1
-        return possibles
-
-    # Diagonal arriba derecha
-    def possible_positions_id(self, row, col):
-        possibles = []
-        next_row, next_col = row - 1, col + 1
-        while next_row >= 0 and next_col < 8:
-            other_piece = self.__board__.get_piece(next_row, next_col)
-            if other_piece is not None:
-                if other_piece.get_color() != self.get_color():
-                    possibles.append((next_row, next_col))
-                break
-            possibles.append((next_row, next_col))
-            next_row -= 1
-            next_col += 1
-        return possibles
-
-    # Diagonal aribba izq
-    def possible_positions_ia(self, row, col):
-        possibles = []
-        next_row, next_col = row - 1, col - 1
-        while next_row >= 0 and next_col >= 0:
-            other_piece = self.__board__.get_piece(next_row, next_col)
-            if other_piece is not None:
-                if other_piece.get_color() != self.get_color():
-                    possibles.append((next_row, next_col))
-                break
-            possibles.append((next_row, next_col))
-            next_row -= 1
-            next_col -= 1
-        return possibles
-
+    def possible_moves(self, from_row, from_col, directions, single_step=False):
+        moves = []
+        for dr, dc in directions:
+            new_row, new_col = from_row, from_col
+            while True:
+                new_row += dr
+                new_col += dc
+                if 0 <= new_row < 8 and 0 <= new_col < 8:
+                    moves.append((new_row, new_col))
+                    if single_step:
+                        break
+                else:
+                    break
+        return moves
