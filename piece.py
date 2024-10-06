ffb5c511533f9d4ae77438e047451de0dee1fcee
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-10-06 10:51:58 -0300

    correccion

--- piece.py
+++ piece.py
@@ -1,38 +1,42 @@
 class Piece:
-    def __init__(self, color,board):
-        self.__color__ = color
-        self.__board__ = board
+    def __init__(self, color, board):
+        self.__color = color
+        self.__board = board
+        self.__row = None
+        self.__col = None
 
-    def __str__(self):
-        if self.__color__ == "WHITE":
-            return self.white_str
-        else:
-            return self.black_str
-        
     def get_color(self):
-        return self.__color__
-    
-    def possible_moves(self, from_row, from_col, directions, single_step=False):
+        return self.__color
+
+    def get_board(self):
+        return self.__board
+
+    def set_position(self, row, col):
+        self.__row = row
+        self.__col = col
+
+    def is_within_board(self, row, col):
+        return 0 <= row < 8 and 0 <= col < 8
+
+    def possible_moves(self, row, col, directions, single_step=False):
         moves = []
         for direction in directions:
-            new_row, new_col = from_row, from_col
-            while True:
-                new_row += direction[0]
-                new_col += direction[1]
-                if 0 <= new_row < 8 and 0 <= new_col < 8:
-                    destination_piece = self.__board__.get_piece(new_row, new_col)
-                    if destination_piece is None:
-                        moves.append((new_row, new_col))
-                    elif destination_piece.get_color() != self.get_color():
-                        moves.append((new_row, new_col))
-                        break
-                    else:
-                        break
+            new_row, new_col = row + direction[0], col + direction[1]
+            while self.is_within_board(new_row, new_col):
+                piece = self.__board.get_piece(new_row, new_col)
+                if piece is None:
+                    moves.append((new_row, new_col))
+                elif piece.get_color() != self.__color:
+                    moves.append((new_row, new_col))
+                    break
                 else:
                     break
-            
                 if single_step:
                     break
+                new_row += direction[0]
+                new_col += direction[1]
         return moves
 
+    def valid_positions(self):
+        raise NotImplementedError("This method should be overridden in subclasses")
   
