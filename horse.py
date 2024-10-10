b35543b19a876d9f5841e41c2c20fdc3a765ec99
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-10-10 09:05:02 -0300

    Correccion

--- horse.py
+++ horse.py
@@ -18,13 +18,5 @@ class Horse(Piece):
         possible_positions = self.possible_moves(from_row, from_col, directions)
         return (to_row, to_col) in possible_positions
 
-    def possible_moves(self, from_row, from_col, directions):
-        moves = []
-        for dr, dc in directions:
-            new_row, new_col = from_row + dr, from_col + dc
-            if 0 <= new_row < 8 and 0 <= new_col < 8:
-                destination_piece = self.__board__.get_piece(new_row, new_col)
-                # Agregar solo posiciones vacías o ocupadas por piezas del oponente
-                if destination_piece is None or destination_piece.get_color() != self.get_color():
-                    moves.append((new_row, new_col))
-        return moves
+    
+        
