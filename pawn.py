bb072ca7bb0cbacc778cd552b7af1f23ae686527
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-10-12 18:06:38 -0300

    Correccion

--- pawn.py
+++ pawn.py
@@ -4,26 +4,29 @@ class Pawn(Piece):
     white_str = "♟"
     black_str = "♙"
 
-    def valid_positions(
-        self,
-        from_row,
-        from_col,
-        to_row,
-        to_col,
-    ):
-    
-       
-        direction = 1 if self.get_color() == "WHITE" else -1  # Determina la dirección del movimiento
-        start_row = 6 if self.get_color() == "WHITE" else 1  # Fila inicial para los peones
+    def valid_positions(self, from_row, from_col, to_row, to_col):
+        direction = -1 if self.get_color() == "WHITE" else 1  # Determina la dirección del movimiento
+        
 
         # Movimiento hacia adelante
-        if from_col == to_col and to_row == from_row + direction:
-            return True  # Movimiento de una casilla hacia adelante
-        if from_col == to_col and from_row == start_row and to_row == from_row + 2 * direction:
-            return True  # Movimiento de dos casillas desde la posición inicial
+        if from_col == to_col:
+            if to_row == from_row + direction:
+                # Movimiento de una casilla hacia adelante
+                if self.get_board().get_piece(to_row, to_col) is None:
+                    return True
+            if from_col == to_col and to_row == from_row + 2 * direction:
+                # Movimiento de dos casillas desde la posición inicial
+                if self.get_board().get_piece(to_row, to_col) is None and self.get_board().get_piece(from_row + direction, from_col) is None:
+                    
+                    return True
 
-        # Captura
+        # Captura en diagonal
         if abs(from_col - to_col) == 1 and to_row == from_row + direction:
-            return True  # Movimiento en diagonal para captura
+            target_piece = self.get_board().get_piece(to_row, to_col)
+            if target_piece is not None and target_piece.get_color() != self.get_color():
+                return True  # Movimiento en diagonal para captura
 
         return False
+
+    def __str__(self):
+        return self.white_str if self.get_color() == "WHITE" else self.black_str
