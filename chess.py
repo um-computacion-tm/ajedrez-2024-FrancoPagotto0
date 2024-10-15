431e6d13b1cf31ad619cd242fc0af8f325b36d25
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-10-15 15:00:12 -0300

    Correccion chess.py y test chess

--- chess.py
+++ chess.py
@@ -1,6 +1,6 @@
 from board import Board
-from exceptions import InvalidMove, InvalidTurn, EmptyPosition
- 
+from exceptions import EmptyPosition, InvalidTurn, InvalidMove
+
 class Chess:
     def __init__(self):
         self.__board__ = Board()
@@ -10,20 +10,19 @@ class Chess:
         return self.__board__
 
     def move(self, from_row, from_col, to_row, to_col):
-        
-        try:
-            piece = self.__board__.get_piece(from_row, from_col)
+        piece = self.__board__.get_piece(from_row, from_col)
 
-            if not piece:
-                raise EmptyPosition() 
-            if piece.get_color() != self.__turn__:
-                raise InvalidTurn() 
-            if not piece.valid_positions(from_row, from_col, to_row, to_col):
-                raise InvalidMove()  
-            self.__board__.move(from_row, from_col, to_row, to_col)
-            self.change_turn()
+        if not piece:
+            raise EmptyPosition() 
+        if piece.get_color() != self.__turn__:
+            raise InvalidTurn("No puedes mover pieza de otro jugador")
+        if not piece.valid_positions(from_row, from_col, to_row, to_col):
+            raise InvalidMove()  
+        self.__board__.move(from_row, from_col, to_row, to_col)
+        self.change_turn()
 
-        except (EmptyPosition, InvalidMove, InvalidTurn) as e:
-           print(str(e)) 
     def change_turn(self):
         self.__turn__ = "BLACK" if self.__turn__ == "WHITE" else "WHITE"
+    
+    def get_turn(self):
+        return self.__turn__
--- test_chess.py
+++ test_chess.py
@@ -3,18 +3,16 @@ from unittest.mock import MagicMock, patch
 from chess import Chess
 from exceptions import InvalidMove, InvalidTurn, EmptyPosition
 
-
-
-
 class TestChess(unittest.TestCase):
 
     @patch('board.Board')
     def setUp(self, MockBoard):
         self.mock_board = MockBoard.return_value
         self.chess = Chess()
+        self.chess._Chess__board__ = self.mock_board  # Asegurarse de que el mock se use en la instancia de Chess
 
     def test_initial_turn(self):
-        self.assertEqual(self.chess._Chess__turn__, "WHITE")
+        self.assertEqual(self.chess.get_turn(), "WHITE")
 
     def test_get_board(self):
         self.assertEqual(self.chess.get_board(), self.mock_board)
@@ -44,16 +42,12 @@ class TestChess(unittest.TestCase):
         mock_piece.get_color.return_value = "WHITE"
         mock_piece.valid_positions.return_value = True
         self.mock_board.get_piece.return_value = mock_piece
-
         self.chess.move(0, 0, 1, 1)
-        self.mock_board.move.assert_called_once_with(0, 0, 1, 1)
-        self.assertEqual(self.chess._Chess__turn__, "BLACK")
+        self.assertEqual(self.chess.get_turn(), "BLACK")
 
     def test_change_turn(self):
         self.chess.change_turn()
-        self.assertEqual(self.chess._Chess__turn__, "BLACK")
-        self.chess.change_turn()
-        self.assertEqual(self.chess._Chess__turn__, "WHITE")
+        self.assertEqual(self.chess.get_turn(), "BLACK")
 
 if __name__ == '__main__':
     unittest.main()
